bc54b6715ff9bb223f2d578a5b6838d6e38392b9
nfzvrni <nfzvrni@gail.com>
2019-03-01 19:49:41 +0530

    Adding source code

---
+++ src/test/java/com/learnmaven/test/FirstMavenTestCase.java
@@ -0,0 +1,33 @@
+package com.learnmaven.test;
+import org.testng.annotations.Test;
+
+public class FirstMavenTestCase {
+	
+	@Test
+	public void A() {
+		// TODO Auto-generated method stub
+		System.out.println("FirstTestCase");
+
+	}
+	
+	@Test
+	public void B() {
+		// TODO Auto-generated method stub
+		System.out.println("SecondTestCase");
+
+	}
+	@Test
+	public void C() {
+		// TODO Auto-generated method stub
+		System.out.println("ThirdTestCase");
+
+	}
+	@Test
+	public void D() {
+		// TODO Auto-generated method stub
+		System.out.println("FourthTestCase");
+
+	}
+	
+
+}
---
+++ src/test/java/com/learnmaven/test/SecondMavenTestCase.java
@@ -0,0 +1,34 @@
+package com.learnmaven.test;
+import org.testng.annotations.Test;
+
+public class SecondMavenTestCase {
+	
+	@Test
+	public void E() {
+		// TODO Auto-generated method stub
+		System.out.println("FirstTestCase S");
+
+	}
+	
+	@Test
+	public void F() {
+		// TODO Auto-generated method stub
+		System.out.println("SecondTestCase S");
+
+	}
+	@Test
+	public void G() {
+		// TODO Auto-generated method stub
+		System.out.println("ThirdTestCase S");
+
+	}
+	@Test
+	public void H() {
+		// TODO Auto-generated method stub
+		System.out.println("FourthTestCase S");
+
+	}
+	
+
+
+}
